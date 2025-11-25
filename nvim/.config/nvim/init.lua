4b165b3fe593650a83bf23246986a0b9de9268a0
Kelvin Zhao <zhao0kelvin@gmail.com>
2025-11-24 20:48:43 -0800

    feat: add git

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -635,7 +635,7 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        ensure_installed = { "pyright" },
         automatic_installation = false,
         handlers = {
           function(server_name)
