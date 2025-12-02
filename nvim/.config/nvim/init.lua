8950eb84d7048361af07d05d0c4d618205f65333
Kelvin Zhao <zhao0kelvin@gmail.com>
2025-12-01 22:09:19 -0800

    feat: add jq binding

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -61,6 +61,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- jq keybindings for JSON processing
+vim.keymap.set("n", "<leader>jq", ":%!jq .<CR>", { desc = "Format entire buffer with jq" })
+
 -- [[ Basic Autocommands ]]
 
 -- Highlight when yanking (copying) text
