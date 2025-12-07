4da26bd611c0a56f1db5ebf8e322bf0f0dbf5dd1
Kelvin Zhao <zhao0kelvin@gmail.com>
2025-12-07 15:22:52 -0800

    feat: use tab to trigger suggestions

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -36,31 +36,19 @@ return {
 	--- @type blink.cmp.Config
 	opts = {
 		keymap = {
-			-- 'default' (recommended) for mappings similar to built-in completions
-			--   <c-y> to accept ([y]es) the completion.
-			--    This will auto-import if your LSP supports it.
-			--    This will expand snippets if the LSP sent a snippet.
-			-- 'super-tab' for tab to accept
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			--
-			-- For an understanding of why the 'default' preset is recommended,
-			-- you will need to read `:help ins-completion`
-			--
-			-- No, but seriously. Please read `:help ins-completion`, it is really good!
-			--
-			-- All presets have the following mappings:
-			-- <tab>/<s-tab>: move to right/left of your snippet expansion
-			-- <c-space>: Open menu or open docs if already open
-			-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-			-- <c-e>: Hide menu
-			-- <c-k>: Toggle signature help
-			--
-			-- See :h blink-cmp-config-keymap for defining your own keymap
 			preset = "default",
-
-			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+			["<C-space>"] = false,
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						return cmp.select_next()
+					else
+						return cmp.show()
+					end
+				end,
+				"fallback", -- If neither above, insert regular Tab
+			},
+			["<S-Tab>"] = { "select_prev", "fallback" },
 		},
 
 		appearance = {
@@ -72,7 +60,7 @@ return {
 		completion = {
 			-- By default, you may press `<c-space>` to show the documentation.
 			-- Optionally, set `auto_show = true` to show the documentation after a delay.
-			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+			documentation = { auto_show = true, auto_show_delay_ms = 500 },
 		},
 
 		sources = {
@@ -97,4 +85,3 @@ return {
 		signature = { enabled = true },
 	},
 }
-
