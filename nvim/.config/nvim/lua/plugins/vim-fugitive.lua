5df8a8712ac217615629e1f46a6c193406f1538d
Kelvin Zhao <zhao0kelvin@gmail.com>
2025-12-07 14:16:10 -0800

    feat: more git bindings

--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -7,7 +7,10 @@ return {
 		{ "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
 		{ "<leader>gP", "<cmd>Git pull<cr>", desc = "Git pull" },
 		{ "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
-		{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff split" },
+		{ "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff" },
+		{ "<leader>gD", "<cmd>Gvdiffsplit!<cr>", desc = "Git 3-way diff" },
 		{ "<leader>gw", "<cmd>Gwrite<cr>", desc = "Git write" },
+		{ "<leader>gh", "<cmd>diffget //2 | diffupdate<cr>", desc = "Git accept target" },
+		{ "<leader>gl", "<cmd>diffget //3 | diffupdate<cr>", desc = "Git accept merge" },
 	},
 }
