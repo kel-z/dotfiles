f6bca899c5d8f1a52855a41bf6961d43344e35b2
Kelvin Zhao <zhao0kelvin@gmail.com>
2026-05-20 03:03:17 -0700

    Revert "feat(nvim): wire up obsession"
    This reverts commit 4863ead4ae29e1f43f4c42206e4f6aad693e3d68.

--- nvim/.config/nvim/lua/plugins/vim-obsession.lua
+++ nvim/.config/nvim/lua/plugins/vim-obsession.lua
@@ -1,11 +1,4 @@
 return {
 	"tpope/vim-obsession",
-	lazy = false,
-	config = function()
-		if vim.fn.filereadable("Session.vim") == 1 then
-			vim.cmd("source Session.vim")
-		elseif vim.env.TMUX then
-			vim.cmd("Obsess")
-		end
-	end,
+	event = "VeryLazy",
 }
